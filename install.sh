289f36c8f1878fb79ca1a1a5adb18d1fa1c37895
bk201c <bk201c@outlook.com>
2022-08-22 10:25:00 +0800

    update bash

--- install.sh
+++ install.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-#github path
+Version="4.7.6"
 GITPATH=https://raw.githubusercontent.com/BK201c/embydd/master/patch
-echo "docker一键破解脚本，适4.7.6版本"
-echo "-------------------web端破解-------------------"
+echo "For v-$Version"
+
+echo "----------Start crack web core ------------------"
 rm -f /system/Emby.Web.dll
 wget -P /system/ $GITPATH/Emby.Web.dll
 rm -f /system/MediaBrowser.Model.dll
 wget -P /system/ $GITPATH/MediaBrowser.Model.dll
-echo "核心破解完成..."
+echo "----------Web Core crack-------done....."
+
+
 rm -f /system/dashboard-ui/modules/emby-apiclient/connectionmanager.js
 wget -P /system/dashboard-ui/modules/emby-apiclient/ $GITPATH/connectionmanager.js
 rm -f /system/dashboard-ui/embypremiere/embypremiere.js
 wget -P /system/dashboard-ui/embypremiere/ $GITPATH/embypremiere.js
-echo "web破解完成..."
+echo "-----------Web crack----done............"
+
 rm -f /system/Emby.Server.Implementations.dll
 wget -P /system/ $GITPATH/Emby.Server.Implementations.dll
-echo "Implementations替换认证..."
+echo "-----------Server core crack----done....."
 
-echo "完成替换..."
+echo "--------------All Done-----enjoy........."
